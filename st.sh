30851bb7280ac3445e1daadeb538f3271e686989
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2025-07-24 13:18:34 +0530

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,9 +1,9 @@
-docker rm -f $(docker ps -aq)
-docker rmi -f $(docker images -q)
-docker system prune -a
 cd
 yum install git docker -y
 systemctl start docker
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -q)
+docker system prune -a
 git clone https://github.com/anitalluri00/Streamlit-EDA-App.git
 cd Streamlit-EDA-App
 docker build -t eda-app .
